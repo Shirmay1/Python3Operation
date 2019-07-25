99cb64b0793f7c3f73ea31203d800ed83f79a1ab
Shirmay1 <398565065@qq.com>
2019-07-26 01:24:37 +0800

    二分查找

---
+++ Algorithm/binary_search.py
@@ -0,0 +1,55 @@
+"""
+日期：2019/7/26
+editor:Shirmay1
+题目：二分查找(两种方法，递归方式，循环方式)
+算法要求：1.必须采用顺序存储结构。2.必须按关键字大小有序排列。
+思路：首先，假设表中元素是按升序排列，将表中间位置记录的关键字与查找关键字比较，如果两者相等，则查找成功；
+        否则利用中间位置记录将表分成前、后两个子表，
+        如果中间位置记录的关键字大于查找关键字，则进一步查找前一子表，否则进一步查找后一子表。
+        重复以上过程，直到找到满足条件的记录，使查找成功，或直到子表不存在为止，此时查找不成功。
+"""
+import random
+
+
+# 递归方式：传入被查找列表，以及需要查找的数
+def binary1(querynum, lis):
+    mid = len(lis) // 2
+    # 第一个判断条件是查看是否为最后一个子表，如果所查找之数且不等于最后一个数，则查找不成功
+    if len(lis) == 1 and querynum != lis[0]:
+        print('所查找的数不在该列表中')
+    elif querynum == lis[mid]:
+        print('查找成功')
+    elif querynum < lis[mid]:
+        binary1(querynum, lis[:mid])
+    # elif querynum > lis[mid]:
+    else:
+        binary1(querynum, lis[mid:])
+
+
+# 非递归方式：传入被查找列表，以及需要查找的数
+def binary2(querynum, lis):
+    while True:
+        mid = len(lis) // 2
+        # 第一个判断条件是查看是否为最后一个子表，如果所查找之数且不等于最后一个数，则查找不成功
+        if len(lis) == 1 and querynum != lis[0]:
+            print('所查找的数不在该列表中')
+            break
+        elif querynum == lis[mid]:
+            print('查找成功')
+            break
+        elif querynum < lis[mid]:
+            lis = lis[:mid]
+            continue
+        # elif querynum > lis[mid]:
+        else:
+            lis = lis[mid:]
+            continue
+
+
+list1 = list(range(1, 100))  # 生成1到100的列表
+list2 = random.sample(list1, 21)  # 从列表1里面挑选21个随机不重复的数
+list3 = sorted(list2)  # 按升序排序得到列表3
+print('查找列表:', list3)
+num = input('请输入要查找的整数：')
+binary1(int(num), list3)
+binary2(int(num), list3)
